c57ed9a26145d99d2f9a5b38017de5c84fbe21c3
Cesar Bacca <cesarbacca@tmtek.net>
2025-03-28 21:36:42 -0500

    feat: agregar nuevos íconos y mejorar la interfaz de configuración en la página de ajustes

--- src/pages/settings/SettingsPage.vue
+++ src/pages/settings/SettingsPage.vue
@@ -2,28 +2,44 @@
 import { defineComponent } from 'vue'
 import { useRouter } from 'vue-router'
 
+// import { useI18n } from 'vue-i18n'
 import AppHeader from '@/components/molecules/header/AppHeader.vue'
 import { IconTypes } from '@/components/molecules/header/enums/icon-types.enum'
+
+import CustomCard from './components/CustomCard.vue'
+import { IconTypesSettings } from './components/enums/icon-types-settings.enum'
+
 
 export default defineComponent({
   components: {
     AppHeader,
+    CustomCard,
   },
   setup() {
     // const { t } = useI18n()
-    const { push } = useRouter()
+    const router = useRouter()
+    const navigateToAudit = () => {
+      router.push({ name: 'report.audit' })
+    }
+    const navigateToMessages = () => {
+      router.push({ name: 'report.message' })
+    }
     return {
-      push,
-      // ActionTypes,
+      router,
       IconTypes,
+      IconTypesSettings,
+      navigateToAudit,
+      navigateToMessages,
     }
   },
 })
 </script>
-<template >
-  <AppHeader
-    :icon="IconTypes.SETTINGS"
-    :text="$t('general.settings')"
-    :actions="[]"
-  />
+<template>
+  <AppHeader :icon="IconTypes.SETTINGS" :text="$t('general.settings')" :actions="[]" />
+  <div class="flex justify-start align-center gap-8 m-4">
+    <CustomCard :icon="IconTypesSettings.CUSTOM_FIELDS" :text="$t('general.personalized_information')"
+      @click="navigateToMessages" />
+    <CustomCard :icon="IconTypesSettings.USERS" :text="$t('user.users')" />
+    <CustomCard :icon="IconTypesSettings.INTEGRATIONS" :text="$t('general.integrations')" @click="navigateToAudit" />
+  </div>
 </template>
---
+++ src/pages/settings/components/CustomCard.vue
@@ -0,0 +1,47 @@
+<script lang="ts">
+import { defineComponent, type FunctionalComponent, type PropType } from 'vue'
+
+import AdministratorsIcon from '@/assets/svg/administrators.svg?component'
+import IntegrationsIcon from '@/assets/svg/integrations.svg?component'
+import InformationIcon from '@/assets/svg/table-actions/information.svg?component'
+
+import { IconTypesSettings } from './enums/icon-types-settings.enum'
+
+
+
+// Map de iconos
+export const IconComponentsSettings: Record<IconTypesSettings, FunctionalComponent> = {
+  [IconTypesSettings.CUSTOM_FIELDS]: InformationIcon,
+  [IconTypesSettings.USERS]: AdministratorsIcon,
+  [IconTypesSettings.INTEGRATIONS]: IntegrationsIcon,
+}
+
+export default defineComponent({
+  props: {
+    icon: {
+      type: String as PropType<keyof typeof IconTypesSettings>,
+      required: false,
+      default: IconTypesSettings.CUSTOM_FIELDS,
+    },
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+  setup() {
+    return {
+      IconComponentsSettings,
+    }
+  },
+})
+</script>
+
+<template>
+  <div
+    class="flex flex-col items-center justify-center w-48 h-38 p-4 border-3 border-gray-900 rounded-md dark:border-white hover:shadow-2xl dark:hover:bg-neutral-700 hover:bg-[var(--p-primary-color)] cursor-pointer">
+    <component :is="IconComponentsSettings[icon]" class="w-12 h-12 dark:fill-white" />
+    <small class="mt-3 text-center text-base text-gray-800 font-semibold dark:text-white">{{
+      text
+    }}</small>
+  </div>
+</template>
